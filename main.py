fa6d2816ddb4b9f3e3f7461ddcacda2cb27e1a24
vasylkartychak <w.i.k.mailua@gmail.com>
2025-07-15 20:04:30 +0300

    base str info

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(2222)
-print(
-    222662662662662662662662662662662662662662662662662662662662662662662662662662662662662662662
-)
-print(222662)
-print(222662)
-print(222662)
-print(222662)
-print(222662)
-print(222662)
+"jghghghghghgh"
+"jhkjhjjhjhj"
+"""jkjkjkjkjkjkjkjkjkjjkjjkjkjkjk😉"""
+
+name = "Alex"
+surname = "Bush"
+print(id(name))
+print(name)
+
+name = name + surname
+
+print(name)
+print(id(name))
+
+symbol = "b"
+print(ord(symbol))
+print(chr(98))
+
+smile = "😉"
+print(ord(smile))
+print(chr(128521))
+print(chr(140000))
+
+heart = "\U00002764jjklkjlkjlkjk"
+print(heart)
+
+my_address = "Odesa"
+ukraine_country_name = "Ukraine"
+PASSWORD = "asddf"
+
+# it is a comment
+
+# adding strings
+string_1 = "-------------str1"
+string_2 = "str2"
+result = string_1 + " " + string_2 + " " + "hjjhjhj"
+print(result)
+
+# multiplication
+mult_result = string_1 * 2
+print(mult_result)
+
+# f strings
+result = f"{string_1} {string_2=} hjjhjhj"
+print(result)
+
+# templates
+WELCOME_TEXT = "Welcome, dear {name}"
+result_ilya = WELCOME_TEXT.format(name="Ilya")
+result_vasya = WELCOME_TEXT.format(name="Vasja")
+print(result_ilya, result_vasya)
